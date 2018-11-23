70c88a236be10aeb63f8643fe919b05d28dcce78
"leehg" <"turboguy36@gmail.com">
2018-11-23 18:11:59 +0900

    get string 수정

--- src/file/to/json/util/GetStringUtil.java
+++ src/file/to/json/util/GetStringUtil.java
@@ -5,37 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class GetStringUtil {
+	public static void main(String[] args) {
+			System.out.println(
+					getStringFromUrl("http://game.bacoder.kr/existId.jsp?&login=suhan").trim());
+	}
 	/**
 	 * HTML 파일을 열어서 안의 내용물을 조회하는 함수
 	 * @param uri
 	 * @return
+	 * @throws IOException 
+	 * @throws MalformedURLException 
 	 */
-	public static String getStringFromUrl(String uri){
-		StringBuilder sb = new StringBuilder();
-		try {
-	        // Create a URL for the desired page
-			URL url = new URL(uri);
-	        // Read all the text returned by the server
-	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-	        
-	        while (in.readLine() != null) {
-	        	sb.append(in.readLine());
-	        }
-	        in.close();
-	        
-	    } catch (MalformedURLException e) {
+	public static String getStringFromUrl(String uri) {
+		String result = new String();
+		try (Scanner scanner = new Scanner(new URL(uri).openStream(),
+	            StandardCharsets.UTF_8.toString()))
+	    {
+	        scanner.useDelimiter("\\A");
+	        result = scanner.hasNext() ? scanner.next() : "";
+	    }catch(MalformedURLException e) {
 	    	e.printStackTrace();
-	    } catch (IOException e) {
+	    }catch(IOException e) {
 	    	e.printStackTrace();
 	    }
-		
-		Document doc = Jsoup.parse(sb.toString()); 
-		String text = doc.body().text();
-		return text;
+		return result;
 	}
 }
