e512c8a36ba1676e7947645ac6871327b0080c07
suhan <suhan@DESKTOP-SU34KMU>
2018-12-07 17:51:41 +0900

    gd

--- src/file/to/json/DictionaryList.java
+++ src/file/to/json/DictionaryList.java
@@ -7,8 +7,12 @@ import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -17,13 +21,18 @@ import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
-public class DictionaryList extends JFrame implements ActionListener{
+public class DictionaryList extends JFrame implements DocumentListener, ActionListener {
+	Logger logger = Logger.getLogger(getClass().getSimpleName());
 	private List<String> data;
-	JTextField textField;
-	JButton bntSeartchButton;
+	private JTextField textField;
+//	JButton bntSeartchButton;
+	JButton oButton;
+	JButton nButton;
 	JList<String> list;
-
+	boolean VocaLsit = false;
 
 	public DictionaryList(GraphicsConfiguration gc) {
 		super(gc);
@@ -52,14 +61,25 @@ public class DictionaryList extends JFrame implements ActionListener{
 		JPanel panel = new JPanel();
 		scrollpane.setColumnHeaderView(panel);
 
-		textField = new JTextField();
+		textField = new JTextField(10);
+		textField.getDocument().addDocumentListener(this);
 		panel.add(textField);
+		
 		textField.setColumns(10);
 
-		bntSeartchButton = new JButton("검색");
-		bntSeartchButton.addActionListener(this);
-		panel.add(bntSeartchButton);
+//		bntSeartchButton = new JButton("검색");
+//		bntSeartchButton.addActionListener(this);
+//		panel.add(bntSeartchButton);
+		
+		oButton = new JButton("오름차순");
+		oButton.addActionListener(this);
+		panel.add(oButton);
 
+		nButton = new JButton("내림차순");
+		nButton.addActionListener(this);
+		panel.add(nButton);
+		
+		
 		DefaultListModel<String> listmodel = new DefaultListModel<>();
 		for(String item : data) {
 			listmodel.addElement(item);
@@ -70,15 +90,17 @@ public class DictionaryList extends JFrame implements ActionListener{
 		list.setModel(listmodel);
 
 	}
-
+/*
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		if(e.getSource() == bntSeartchButton) {
 			System.out.println(textField.getText());
 			filterModel((DefaultListModel<String>)list.getModel(), textField.getText());
+			
 		}
 	}
+	*/
+
 	public void filterModel(DefaultListModel<String>model, String filiter) {
 		Iterator<String> iter = data.iterator();
 		while(iter.hasNext()) {
@@ -86,6 +108,7 @@ public class DictionaryList extends JFrame implements ActionListener{
 			if(item .contains(filiter)) {
 				if(!model.contains(item)) {
 					model.addElement(item);
+					
 				}
 			}else {
 				if(model.contains(item)) {
@@ -94,6 +117,57 @@ public class DictionaryList extends JFrame implements ActionListener{
 			}
 		}
 	}
+	
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		String txtfiled= textField.getText();
+		
+			filterModel((DefaultListModel<String>)list.getModel(), textField.getText());
+		
+		 
+	}
+	
+	
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == oButton) {
+			Collections.sort(data);
+			DefaultListModel<String> listModel = (DefaultListModel<String>)list.getModel();
+			listModel.removeAllElements();
+			for(String item : data) {
+				listModel.addElement(item);
+			}
+		}
+		else if(e.getSource() == nButton){
+			Collections.sort(data,new DescendingString());
+			DefaultListModel<String> listModel = (DefaultListModel<String>)list.getModel();
+			listModel.removeAllElements();
+			for(String item : data) {
+				listModel.addElement(item);
+			}
+		}
+	}
+	
+	class DescendingString implements Comparator<String>{
+		@Override
+		public int compare(String a, String b) {
+			return b.compareToIgnoreCase(a);
+		}
+	}
+	
 }
+
 
 
